f7586981466b7a86ffd04ac7a36d4d48fb73ae25
ajayrajput74 <arajput391998@gmail.com>
2024-04-05 13:21:16 +0530

    adding first commit

---
+++ src/main/java/com/example/controller/DockerDemoController.java
@@ -0,0 +1,19 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "docker")
+public class DockerDemoController {
+
+	@GetMapping(value = "value")
+	public String getData() {
+      return "Docker Image Runing";
+	}
+	
+	
+	
+
+}
